a333578b03e5a7cb39e97b809d2af3bf8b88f4cc
Perroscky <la.samaniegos@uea.edu.ec>
2024-12-14 10:22:33 -0500

    Merge remote-tracking branch 'origin/main'

--- Unidad 1/1.3. Comparación de Programación Tradicional y POO en Python/1.3.1. Programación Tradicional.py
+++ Unidad 1/1.3. Comparación de Programación Tradicional y POO en Python/1.3.1. Programación Tradicional.py
@@ -1,4 +1,11 @@
 def ingresar_temperaturas_semanales():
+    """
+    Esta función solicita al usuario que ingrese las temperaturas de una semana y
+    las almacena en una lista.
+
+    Returns:
+        list: Una lista que contiene las temperaturas ingresadas.
+    """
     temperaturas = []
     for dia in range(1, 8):
         temperatura = float(input(f"Ingrese la temperatura del día {dia}: "))
@@ -6,8 +13,18 @@ def ingresar_temperaturas_semanales():
     return temperaturas
 
 def calcular_promedio(temperaturas):
+    """
+    Calcula el promedio de una lista de temperaturas.
+
+    Args:
+        temperaturas (list): Una lista de temperaturas.
+
+    Returns:
+        float: El promedio de las temperaturas.
+    """
     return sum(temperaturas) / len(temperaturas)
 
+# Bloque principal del programa
 temperaturas_semana = ingresar_temperaturas_semanales()
 promedio_semanal = calcular_promedio(temperaturas_semana)
 
